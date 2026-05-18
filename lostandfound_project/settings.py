c8660c3a03ab6e8801305e05c3c510c2753adad2
astsuganya <141382963+astsuganya@users.noreply.github.com>
2026-05-19 00:23:57 +0700

    Edit for Deploying

--- lostandfound_project/settings.py
+++ lostandfound_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8j=97bq!+qzt)$0fgyy-kr7$=a*6g^c3^-)(x1l+c%4mb(_)nz'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8j=97bq!+qzt)$0fgyy-kr7$=a*6g^c3^-)(x1l+c%4mb(_)nz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
