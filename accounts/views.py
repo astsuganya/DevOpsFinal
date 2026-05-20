b17127ef26af9643ea07236145c7742e37400ba6
astsuganya <141382963+astsuganya@users.noreply.github.com>
2026-05-20 22:31:53 +0700

    Update Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, update_session_auth_hash, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import PasswordChangeForm
 
 from core.models import Item, Comment
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('core:item_list')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
 
